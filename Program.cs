453e8e60dcf0984be58acb6ce5708fd574885521
Walter Nogueira <netoneto796@gmail.com>
2023-05-19 19:06:36 -0300

    Its working, but need some improviments

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using EnglishTutor.Entities;
+
+namespace EnglishTutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenAITutor  tutor = new OpenAITutor();
+
+            while (true) 
+            {
+                Console.WriteLine("What is your question?");
+                string prompt = Console.ReadLine();
+
+                if (prompt.ToLower() == "close")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(tutor.question(prompt));
+                }
+            }
+        }
+    }
+}
