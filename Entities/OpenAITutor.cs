4d4f82fecf707397f4215aa30230c6b161497e5a
Walter Nogueira <netoneto796@gmail.com>
2023-05-23 12:02:05 -0300

    Revert "Algumas alteracoes foram feitas, a api não vai ficar aqui"
    This reverts commit 35fa5fefc7891cac49e59225f9d3e845ae9235a1.

---
+++ Entities/OpenAITutor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnglishTutor.Entities
+{
+    internal class OpenAITutor
+    {
+        private string _apiKey = "API_KEY";
+
+        public async Task question(string message)
+        {
+            if (String.IsNullOrWhiteSpace(message))
+            {
+                Console.Clear();
+                Thread.Sleep(2000);
+                throw new ArgumentNullException("You need to ask something!");
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + _apiKey);
+                var response = await client.PostAsync("https://api.openai.com/v1/completions",
+                new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + message + "\", \"temperature\": 1, \"max_tokens\": 1024}", Encoding.UTF8, "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseStg = await response.Content.ReadAsStringAsync();
+                    Response data = JsonSerializer.Deserialize<Response>(responseStg);
+
+                    Array.ForEach(data.choices.ToArray(), (item) => Console.WriteLine(item.text.Replace("\n", "")));
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+
+        }
+    }
+}
