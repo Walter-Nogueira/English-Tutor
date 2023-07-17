9ac0580006898740339df79c4244eebf7a6b8ca7
Walter Nogueira <netoneto796@gmail.com>
2023-07-17 15:17:59 -0300

    The modol was finalized, it still needs some adjustments that will be done in the future

--- frontend/src/scripts/modal.js
+++ frontend/src/scripts/modal.js
@@ -1,18 +1,19 @@
 const signUp = document.getElementById('menu-sign-up');
-const Login = document.getElementById('menu-login');
-const modalSignUp = document.getElementById('modal-sign-up');
+const modal = document.getElementById('modal');
 const btnCloseLogin = document.getElementById('btn-close-login');
 const imgModal = document.getElementById('img-modal');
 const dynamicContent = document.getElementById('dynamic-container');
 
 function OpenModal(){
-    modalSignUp.showModal();
-    modalSignUp.style.display = 'inline';
+    modal.showModal();
+    modal.style.display = 'inline';
 };
 
 function CloseModal(){
-    modalSignUp.close();
-    modalSignUp.style.display = 'none';
+    modal.close();
+    modal.style.display = 'none';
+    dynamicContent.innerHTML = '';
+    imgModal.innerHTML = '';
 };
 
 document.addEventListener("keydown", function(event) {
@@ -23,19 +24,48 @@ document.addEventListener("keydown", function(event) {
 
 signUp.addEventListener('click', () => {
     OpenModal();
-    if(dynamicContent.childElementCount > 0){
-        dynamicContent.innerHTML = '';
-        CreateSignUp();
-    }
-    else{
-        CreateSignUp();
-    }
+    CreateSignUp();
 });
 
 function ApplyStyles(element, style){
     for (let prop in style){
         element.style[prop] = style[prop];
     }
+}
+
+function CreateTitle(title, subtitle){
+    const div = document.createElement('div');
+    div.id = 'title';
+    div.classList.add('title');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = title;
+
+    const p = document.createElement('p');
+    p.textContent = subtitle;
+
+    div.appendChild(h1);
+    div.appendChild(p)
+
+    const titleStyle = {
+        width: '70%',
+        height: 'auto',
+        display: 'flex',
+        flexDirection:'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        margin: '0px 0px 0px 30px',
+    }
+
+    ApplyStyles(div, titleStyle);
+
+    h1.style.fontSize = '1.7em';
+    h1.style.fontWeight = '800';
+
+    p.style.fontSize = '0.9em';
+    p.style.color = 'var(--color-gray2)';
+
+    return div;
 }
 
 function CreateTextField(labelContent, type, name, autocomplete, placeholder, required){
@@ -87,26 +117,64 @@ function CreateTextField(labelContent, type, name, autocomplete, placeholder, re
     return div;
 }
 
-function createGoogleButton(value){
+function CreateSubmitButton(value){
+    const submit = document.createElement('div');
+    submit.classList.add('btn-submit');
+    submit.style.width = '60%';
 
-    const fragment = document.createDocumentFragment();
+    const submitBtn = document.createElement('button');
+    submitBtn.type = 'submit';
+    submitBtn.textContent = value;
+    submit.appendChild(submitBtn);
+    
+    return submit;
+}
 
-    const other = document.createElement('div');
-    other.classList.add('other');
-  
-    const lines = [];
-    for (let i = 0; i < 2; i++) {
-      const line = document.createElement('div');
-      line.classList.add('line');
-      lines.push(line);
+function Divider(){
+    const divider = document.createElement('div');
+    divider.classList.add('divider');
+    
+    const dividerStyle = {
+        width: '100%', 
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '0.9em',
+        color: 'var(--color-gray2)',
+        gap: '10px'
     }
-  
-    other.appendChild(lines[0]);
-    other.appendChild(document.createElement('p')).textContent = 'Or';
-    other.appendChild(lines[1]);
-  
-    fragment.appendChild(other);
-  
+
+    ApplyStyles(divider, dividerStyle);
+
+    const dividerLines = [];
+    for(let i = 0; i < 2; i++){
+        let line = document.createElement('hr');
+        line.classList.add('divider-line');
+        dividerLines.push(line);
+
+        const lineStely = {
+            width: '26%',
+            height: '1px',
+            border: 'none',
+            backgroundColor: 'var(--color-gray3)'
+        };
+        
+        ApplyStyles(line, lineStely);
+    }
+
+    const span = document.createElement('span');
+    span.classList.add('divider-text');
+    span.textContent = 'Or';
+
+    divider.appendChild(dividerLines[0]);
+    divider.appendChild(span);
+    divider.appendChild(dividerLines[1]);
+    
+    return divider;
+}
+
+function createGoogleButton(value){
     const googleSubmit = document.createElement('div');
     googleSubmit.classList.add('google-submit');
   
@@ -136,65 +204,37 @@ function createGoogleButton(value){
   
     googleSubmit.appendChild(googleSubmitInput);
   
-    fragment.appendChild(googleSubmit);
-  
-    return fragment;
+    return googleSubmit;
 }
-
-function CreateTitle(title, subtitle){
-    const div = document.createElement('div');
-    div.id = 'title';
-    div.classList.add('title');
-
-    const h1 = document.createElement('h1');
-    h1.textContent = title;
-
-    const p = document.createElement('p');
-    p.textContent = subtitle;
-
-    div.appendChild(h1);
-    div.appendChild(p)
-
-    const titleStyle = {
-        width: '70%',
-        height: 'auto',
-        display: 'flex',
-        flexDirection:'column',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        margin: '0px 0px 0px 30px',
-    }
-
-    ApplyStyles(div, titleStyle);
-
-    h1.style.fontSize = '1.7em';
-    h1.style.fontWeight = '800';
-
-    p.style.fontSize = '0.9em';
-    p.style.color = 'var(--color-gray2)';
-
-    return div;
-}
-
 
 function CreateSignUp(){
-    
-    const formContent = document.createDocumentFragment();
+    const img = document.createElement('img');
+    img.src = '../assets/svg_modal/English teacher-amico.svg';
+    img.alt = 'English teacher';
+    imgModal.appendChild(img);
+   
+    const fragment = document.createDocumentFragment();
 
-    if (imgModal.childElementCount > 0) {
-        const imgElement = imgModal.firstElementChild;
-        imgElement.src = '../assets/svg_modal/English teacher-amico.svg';
-        imgElement.alt = 'English teacher';
-    } else {
-        const img = document.createElement('img');
-        img.src = '../assets/svg_modal/English teacher-amico.svg';
-        img.alt = 'English teacher';
-        imgModal.appendChild(img);
-    }
+    let title = 'Get started';
+    let subtitle = 'Create your account and start learning english today';
+    const titleContent = CreateTitle(title,subtitle);
 
-    const title = 'Get started';
-    const subtitle = 'Creat your account and start learning english today';
-    formContent.appendChild(CreateTitle(title, subtitle));
+    fragment.appendChild(titleContent);
+
+    const form = document.createElement('form');
+    form.classList.add('form-content');
+    form.id = 'form-content';
+    form.method = 'post';
+
+    const formStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        margin: '20px 0px'
+    };
+
+    ApplyStyles(form, formStyle);
 
     const textFieldAttributes = [
         {labelContent: "Full name", type: "text", name: "user", autocomplete: "name", placeholder: "Hideo Kojima", required: true},
@@ -203,11 +243,10 @@ function CreateSignUp(){
     ];
 
     textFieldAttributes.forEach(obj => {
-        formContent.appendChild(CreateTextField(obj.labelContent, obj.type, obj.name, obj.autocomplete, obj.placeholder, obj.required));
-    });
+        form.appendChild(CreateTextField(obj.labelContent, obj.type, obj.name, obj.autocomplete, obj.placeholder, obj.required));
+    })
 
-    const terms = document.createElement('div');
-    terms.classList.add('terms');
+    fragment.appendChild(form);
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -216,55 +255,137 @@ function CreateSignUp(){
     checkbox.value = 'terms-and-cond';
     checkbox.required = true;
 
-    terms.appendChild(checkbox);
+    const  termsContainer= document.createElement('div');
+    termsContainer.classList.add('terms-container');
 
-    const termsParagraph = document.createElement('p');
-    termsParagraph.textContent = 'I accept the ';
+    termsContainerStyle = {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '5px 0px -15px 0px',
+        width: '100%',
+        gap: '5px',
+        fontSize: '0.8em'
+    }
 
-    const termsLink = document.createElement('a');
-    termsLink.href = '#';
-    termsLink.textContent = 'terms and conditions';
+    ApplyStyles(termsContainer, termsContainerStyle);
 
-    termsParagraph.appendChild(termsLink);
-    terms.appendChild(termsParagraph);
-    formContent.appendChild(terms);
+    termsContainer.appendChild(checkbox);
 
-    const submit = document.createElement('div');
-    submit.classList.add('btn-submit');
+    let paragraphTerms = '<p>I accept the <a href="#">terms of use</a>.</p>';
+    termsContainer.insertAdjacentHTML('beforeend', paragraphTerms);
+    const p = termsContainer.querySelector('p');
+    const a = p.querySelector('a');
+    a.style.color = 'var(--color-mediumTealBlue)';
+    form.appendChild(termsContainer);
 
-    const submitInput = document.createElement('input');
-    submitInput.type = 'submit';
-    submitInput.value = 'Create an account';
-    submit.appendChild(submitInput);
-    formContent.appendChild(submit);
+    const HaveAccount = document.createElement('div');
+    HaveAccount.classList.add('paragraph')
+    HaveAccount.innerHTML = '<p>Have an account? <a href="#">Login</a></p>';
+    
+    fragment.appendChild(CreateSubmitButton('Create an account'))
+    fragment.appendChild(Divider());
+    fragment.appendChild(createGoogleButton('Sign up with Google'))
+    fragment.appendChild(HaveAccount);
+
+    dynamicContent.appendChild(fragment);
+}
+
+const login = document.getElementById('menu-login');
+
+login.addEventListener('click', () =>{
+    OpenModal();
+    CreateLogin();
+})
+
+function CreateLogin(){
+
+    const img = document.createElement('img');
+    img.src = "../assets/svg_modal/disabled student-amico.svg";
+    img.alt = 'Studying';
+    imgModal.appendChild(img);
+    
+    const fragment = document.createDocumentFragment();
+
+    const title = 'English learnig hub';
+    const subtitle = 'Login to embrace a new level of fluency';
+    let divTitle = CreateTitle(title, subtitle)
+    divTitle.style.margin = '20px 0px 10px 30px';
+    
+    fragment.appendChild(divTitle);
+
+    const textFieldAttributes = [
+        {labelContent: "Email", type: "email", name: "email", autocomplete: "email", placeholder: "hkojima_69@example.com", required: true},
+        {labelContent: "Password", type: 'password', name: 'password', autocomplete: 'password', placeholder: '***********', required: true}
+    ];
 
     const form = document.createElement('form');
     form.classList.add('form-content');
     form.id = 'form-content';
     form.method = 'post';
-    form.appendChild(formContent);
 
     const formStyle = {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
         width: '100%',
-        flexGrow: '2'
+        margin: '20px 0px'
     };
 
     ApplyStyles(form, formStyle);
 
-    dynamicContent.appendChild(form);
-    dynamicContent.appendChild(createGoogleButton('Sign up with Google'));
+    textFieldAttributes.forEach(obj => {
+        form.appendChild(CreateTextField(obj.labelContent, obj.type, obj.name, obj.autocomplete, obj.placeholder, obj.required));
+    });
 
-    const haveAccount = document.createElement('div');
-    haveAccount.classList.add('paragraph');
+    fragment.appendChild(form);
 
-    const p = document.createElement('p');
-    p.innerHTML = 'Have an account? <a href="#">Login</a>';
+    function RememberMe(){
+        const rememberMeCheckbox = document.createElement('div');
+        rememberMeCheckbox.classList.add('remember-me-checkbox');
 
-    haveAccount.appendChild(p);
-    dynamicContent.appendChild(haveAccount);
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'remember-me';
+        checkbox.id = 'remember-me';
+        checkbox.value = 'remember-me';
+        checkbox.required = false;
 
+        rememberMeCheckbox.appendChild(checkbox);
+
+        let Paragraph = '<p>Remember me</p>';
+        rememberMeCheckbox.insertAdjacentHTML('beforeend', Paragraph);
+
+        return rememberMeCheckbox;
+    }
+    
+    function ForgotPassword(){
+        const forgotPasswordLink = document.createElement('div');
+        forgotPasswordLink.classList.add('forgot-password-link');
+
+        forgotPasswordLink.innerHTML= '<a href="#">Forgot password?</a>';
+
+        return forgotPasswordLink
+    }
+
+
+    const optionContainer = document.createElement('div');
+    optionContainer.classList.add('option-container');
+    optionContainer.appendChild(RememberMe());
+    optionContainer.appendChild(ForgotPassword());
+    form.appendChild(optionContainer);
+    
+    fragment.appendChild(CreateSubmitButton('Login'));
+    fragment.appendChild(Divider());
+    fragment.appendChild(createGoogleButton('Login with Google'))
+
+    let createAccount = document.createElement('div');
+    createAccount.classList.add('paragraph');
+    createAccount.innerHTML = '<p>Don`t have an account? <a href="#">Sign up</a></p>';
+
+    fragment.appendChild(createAccount);
+
+    dynamicContent.appendChild(fragment);
 }
+
